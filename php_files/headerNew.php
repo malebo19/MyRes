34c94f5ccb1f7e9f082ac65aef39829bd3835189
AngelicSpace <angelicdnll@gmail.com>
2021-05-26 13:52:48 +0200

    Create headerNew.php

---
+++ php_files/headerNew.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" type="text/css" href="..\css_files\header.css">
+
+<header class="row" style="height: 15%;">
+  
+  <div class="col-lg-6">
+    <img src="..\pictures\logoNew.jpeg" id="logoImg" style="float: left;"> 
+    <span id="resName" style="float: left; margin-top:2%; margin-left:2%; ">MyRes</span>
+  </div>
+
+ 
+  <div class="row col-lg-6">
+    <div class="email" style="margin-left: 2%; margin-top:2%; color:white;" >company@email.com</div>
+    <button id="loginButton" class="col-lg-3" style="float: right; width:10%; margin-top:-2%;">Login</button>
+  </div>
+
+</header> 
